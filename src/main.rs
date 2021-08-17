82222befab731d4779b07d85429ee2654fbf0b2f
Lucas Taylor <lucas@perfectlunacy.com>
2021-08-17 13:33:02 -0700

    Runs shell commands interactively

--- src/main.rs
+++ src/main.rs
@@ -2,51 +2,79 @@ use std::sync::mpsc::{channel, Receiver, TryRecvError};
 use std::thread;
 
 use rex::child::ChildProcess;
-use std::io::Read;
+use std::io::{Read, Write, stdout};
+use log::info;
+use simplelog::{CombinedLogger, WriteLogger, LevelFilter, Config};
+use std::fs::File;
+use termion::raw::IntoRawMode;
+use bytes::Bytes;
 
 mod rex;
 
 /***
-    A non-blocking read that's ok with an empty buffer
-     ***/
-fn read_output(rx: &mut Receiver<String>) -> anyhow::Result<String> {
+A non-blocking read that's ok with an empty buffer
+ ***/
+fn read_output<T>(rx: &mut Receiver<T>) -> anyhow::Result<Option<T>> {
     match rx.try_recv() {
-        Ok(s) => Ok(s),
-        Err(TryRecvError::Empty) => Ok(String::new()),
+        Ok(s) => Ok(Some(s)),
+        Err(TryRecvError::Empty) => Ok(None),
         Err(e) => Err(e.into())
     }
 }
 
 fn run() -> anyhow::Result<()> {
+    init_logging()?;
+
     let (output_tx, mut output_rx) = channel();
-    let mut proc = ChildProcess::new(output_tx);
+    let (status_tx, mut status_rx) = channel();
+    let mut proc = ChildProcess::new(output_tx, status_tx);
     let input_tx = proc.input_tx();
 
     thread::spawn( move || {
-        proc.run().expect("Child crashed!");
+        proc.run().expect("Child crashed");
+        info!("Process exited! Shutdown.");
+        proc.shutdown().unwrap();
     });
 
     let mut stdin = termion::async_stdin();
-    loop {
+    let mut stdout = stdout().into_raw_mode()?;
+
+    while None == read_output(&mut status_rx)? {
         // read stdin and forward it to the proc.
         let mut input = String::new();
         stdin.read_to_string(&mut input)?;
-        if !input.is_empty() { input_tx.send(input)?; }
+        if !input.is_empty() {
+            info!("Read input: {}", input.as_str());
+            input_tx.send(input)?;
+        }
 
         // read stdout and display it
-        print!("{}", read_output(&mut output_rx)?)
+        let output = String::from_utf8(read_output(&mut output_rx)?.unwrap_or(Bytes::from(&b""[..])).to_vec())?;
+        write!(stdout, "{}", output)?;
+        stdout.flush()?;
     }
+
+    Ok(())
+}
+
+fn init_logging() -> anyhow::Result<()> {
+    CombinedLogger::init(
+        vec![
+            WriteLogger::new(LevelFilter::Info, Config::default(), File::create("log/hex.log")?),
+        ]
+    )?;
+
+    Ok(())
 }
 
 fn main() {
-
     // Create a master session
     // Spawn a child process in another thread
     //   give it the appropriate halves of Input/Output channels
     // Input Thread: Forward stdin to the child's Input channel
     // Output Thread: Forward stdout from the child to the Output channel
     match run() {
-        Ok(_) => { print!("{}", "Shutting down!") },
+        Ok(_) => { print!("{}", "Shutdown!") },
         Err(err) => { print!("{:?}", err)}
     }
 }
--- src/rex/child/mod.rs
+++ src/rex/child/mod.rs
@@ -3,23 +3,35 @@
 * Keeps track of all the things we need for trafficing I/O between processes
 ***/
 use std::sync::mpsc::{Receiver, Sender, channel, TryRecvError};
-use std::process::{Command, Child};
-use std::io::{Write, Read};
+use std::io::{Read, Write};
+use log::info;
+use portable_pty::{CommandBuilder, PtySize, native_pty_system, PtyPair};
+use bytes::Bytes;
 
 pub struct ChildProcess {
+    shutdown: bool,
     input_receiver: Receiver<String>,
     input_sender: Sender<String>,
-    output_sender: Sender<String>,
+    pub output_sender: Sender<Bytes>,
+    pub status_sender: Sender<String>,
 }
 
 impl ChildProcess {
-    pub fn new(out_tx: Sender<String>) -> ChildProcess {
+    pub fn new(out_tx: Sender<Bytes>, status_tx: Sender<String>) -> ChildProcess {
         let (in_tx, in_rx) = channel();
         ChildProcess {
+            shutdown: false,
             input_receiver: in_rx,
             input_sender: in_tx,
             output_sender: out_tx,
+            status_sender: status_tx
         }
+    }
+
+    pub fn shutdown(&mut self) -> anyhow::Result<()> {
+        self.status_sender.send("shutdown".to_owned())?;
+        self.shutdown = true;
+        Ok(())
     }
 
     /***
@@ -33,25 +45,34 @@ impl ChildProcess {
     Launches the child's process and runs until the process exits
      */
     pub fn run(&mut self) -> anyhow::Result<()> {
-        let mut child: Child = self.launch()?;
+        let mut child = self.launch()?;
 
-        while let None = child.try_wait()? {
+        // forward output
+        let mut reader = child.master.try_clone_reader()?;
+        let sender = self.output_sender.clone();
+        std::thread::spawn( move || {
+            loop {
+                let mut output = [0u8; 1024];
+                let size = reader.read(&mut output).unwrap_or(0);
+                sender.send(Bytes::from(output[..size].to_owned())).unwrap();
+            };
+        });
+
+        loop {
+            if self.shutdown {
+                info!("received shutdown sequence - leaving");
+                break;
+            }
+
             // Still running - process I/O!
 
-            // Send input
+            // Consume input
             let input = self.read_input()?;
-            if !input.is_empty() {
-                if child.stdin.is_some() {
-                    child.stdin.take().unwrap().write(input.as_bytes())?;
-                }
-            }
+            info!("rcvd {}", input);
+            write!(child.master, "{}", input)?;
+            child.master.flush()?;
 
-            // forward output
-            let mut output = String::new();
-            if child.stdout.is_some() {
-                child.stdout.take().unwrap().read_to_string(&mut output)?;
-            }
-            self.output_sender.send(output)?;
+
         }
 
         Ok(())
@@ -62,21 +83,34 @@ impl ChildProcess {
      ***/
     fn read_input(&mut self) -> anyhow::Result<String> {
         match self.input_receiver.try_recv() {
-            Ok(s) => Ok(s),
+            Ok(s) => {
+                info!("rcvd input: {}", s.as_str());
+                Ok(s)
+            },
             Err(TryRecvError::Empty) => Ok(String::new()),
             Err(e) => Err(e.into())
         }
     }
 
-    fn launch(&self) -> anyhow::Result<Child>{
-        let child = Command::new("/usr/bin/bash")
-            .current_dir("/home/lucas")
-            .spawn()
-            .expect("Failed to launch child");
+    fn launch(&self) -> anyhow::Result<PtyPair> {
+        let pty_sys = native_pty_system();
+        let pair = pty_sys.openpty(PtySize {
+            rows: 24,
+            cols: 80,
+            // Not all systems support pixel_width, pixel_height,
+            // but it is good practice to set it to something
+            // that matches the size of the selected font.  That
+            // is more complex than can be shown here in this
+            // brief example though!
+            pixel_width: 0,
+            pixel_height: 0,
+        })?;
 
-        Ok(child)
+        let cmd = CommandBuilder::new("/usr/bin/bash");
+
+        pair.slave.spawn_command(cmd)?;
+        Ok(pair)
     }
-
 }
 
 /***
