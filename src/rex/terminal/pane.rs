2b8e19d9ef771bba384c58421fa887302084f500
Lucas Taylor <lucas@perfectlunacy.com>
2021-09-07 10:57:30 -0700

    Make PrintState serialize back to VT100 codes...badly

--- src/rex/terminal/pane.rs
+++ src/rex/terminal/pane.rs
@@ -1,4 +1,4 @@
-use crate::rex::terminal::vt100_string::VT100String;
+use crate::rex::terminal::vt100_string::{VT100String, Glyph};
 use regex::{Regex, Captures};
 
 pub struct Pane {
@@ -17,11 +17,11 @@ pub struct Pane {
     cur_y: usize,
 
     // current print state
-    print_state: PrintState
+    print_state: PrintState,
 }
 
-#[derive(Eq, PartialEq, Debug)]
-enum Color {
+#[derive(Eq, PartialEq, Debug, Copy, Clone)]
+pub enum Color {
     Black,
     Red,
     Green,
@@ -31,36 +31,93 @@ enum Color {
     Cyan,
     White,
     TWOFIFTYSIX(u8),
-    RGB(u8, u8, u8)
+    RGB(u8, u8, u8),
 }
 
-struct PrintState {
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub struct PrintState {
     pub foreground: Color,
     pub background: Color,
     pub underline: bool,
     pub blink: bool,
-    pub bold: bool
+    pub bold: bool,
+}
+
+impl PrintState {
+    pub fn to_str(&self) -> String {
+        // TODO: Assemble a set of numbers to push together into a single command.
+
+        // Add colors first
+        let fg_base = if self.bold { 90 } else { 30 };
+        let bg_base = if self.bold { 100 } else { 40 };
+
+        let fg_str = match self.foreground {
+            Color::TWOFIFTYSIX(num) => { format!("\x1b[38;5;{}m", num) }
+            Color::RGB(r, g, b) => { format!("\x1b[38;2;{};{};{}m", r, g, b) }
+            color => { format!("\x1b[{}m", fg_base + color.to_offset()) }
+        };
+
+        let bg_str = match self.background {
+            Color::TWOFIFTYSIX(num) => { format!("\x1b[38;5;{}m", num) }
+            Color::RGB(r, g, b) => { format!("\x1b[38;2;{};{};{}m", r, g, b) }
+            color => { format!("\x1b[{}m", bg_base + color.to_offset()) }
+        };
+
+        let blink = if self.blink {
+            "\x1b[5m"
+        } else {
+            ""
+        };
+
+        let underlined = if self.underline {
+            "\x1b[4m"
+        } else {
+            ""
+        };
+
+        let mut out = String::from(fg_str);
+        out.push_str(&bg_str);
+        out.push_str(&blink);
+        out.push_str(&underlined);
+
+        out
+    }
 }
 
 impl Color {
     pub fn eight_color(base: u8) -> Color {
         match base % 10 {
-            0 => { Color::Black },
-            1 => { Color::Red },
-            2 => { Color::Green },
-            3 => { Color::Yellow },
-            4 => { Color::Blue },
-            5 => { Color::Magenta },
-            6 => { Color::Cyan },
-            _ => { Color::White },
+            0 => { Color::Black }
+            1 => { Color::Red }
+            2 => { Color::Green }
+            3 => { Color::Yellow }
+            4 => { Color::Blue }
+            5 => { Color::Magenta }
+            6 => { Color::Cyan }
+            _ => { Color::White }
         }
     }
 
+    pub fn to_offset(&self) -> u8 {
+        match self {
+            Color::Black => { 0 }
+            Color::Red => { 1 }
+            Color::Green => { 2 }
+            Color::Yellow => { 3 }
+            Color::Blue => { 4 }
+            Color::Magenta => { 5 }
+            Color::Cyan => { 6 }
+            Color::White => { 7 }
+            _ => { 255 }
+        }
+    }
+
+
     pub fn extended_color(args: &[u8]) -> anyhow::Result<Color> {
         match args.first().unwrap() {
-            2 => { Ok(Color::RGB(args[1], args[2], args[3])) },
+            2 => { Ok(Color::RGB(args[1], args[2], args[3])) }
             5 => { Ok(Color::TWOFIFTYSIX(args[1])) }
-            _ => { panic!("{} is not a valid SGR extended color argument!", args.first().unwrap())}
+            _ => { panic!("{} is not a valid SGR extended color argument!", args.first().unwrap()) }
         }
     }
 }
@@ -72,14 +129,14 @@ impl Default for PrintState {
             background: Color::Black,
             underline: false,
             blink: false,
-            bold: false
+            bold: false,
         }
     }
 }
 
 impl PrintState {
     pub fn apply_vt100(&mut self, s: &str) -> anyhow::Result<()> {
-        let parm_rx = Regex::new("\x1b]([0-9;]+)m").unwrap();
+        let parm_rx = Regex::new("\x1b\\[([0-9;]+)m").unwrap();
         match parm_rx.captures(s) {
             None => { panic!("{} does not look like an SGR sequence!", s) }
             Some(captures) => {
@@ -97,23 +154,23 @@ impl PrintState {
                         self.blink = false;
                         self.underline = false;
                         self.bold = false;
-                    },
-                    1 => { self.bold = true; },
-                    2 => { self.bold = false; },
-                    4 => { self.underline = true; },
-                    5 => { self.blink = true; },
-                    30..=37 => { self.foreground = Color::eight_color(*sgr_code); },
-                    38 => { self.foreground = Color::extended_color(&int_parts[1..])? },
-                    40..=47 => { self.background = Color::eight_color(*sgr_code); },
-                    48 => { self.background = Color::extended_color(&int_parts[1..])? },
+                    }
+                    1 => { self.bold = true; }
+                    2 => { self.bold = false; }
+                    4 => { self.underline = true; }
+                    5 => { self.blink = true; }
+                    30..=37 => { self.foreground = Color::eight_color(*sgr_code); }
+                    38 => { self.foreground = Color::extended_color(&int_parts[1..])? }
+                    40..=47 => { self.background = Color::eight_color(*sgr_code); }
+                    48 => { self.background = Color::extended_color(&int_parts[1..])? }
                     90..=97 => {
                         self.foreground = Color::eight_color(*sgr_code);
                         self.bold = true;
-                    },
+                    }
                     100..=107 => {
                         self.background = Color::eight_color(*sgr_code);
                         self.bold = true;
-                    },
+                    }
 
                     _ => { panic!("Invalid or unknown SGR code {}", sgr_code) }
                 }
@@ -127,16 +184,18 @@ impl PrintState {
 
 impl Pane {
     pub fn new(id: &str, x: u16, y: u16, height: u16, width: u16) -> Pane {
-        let mut lines = (0..height).map(|_| VT100String::new("")).collect::<Vec<VT100String>>();
+        let mut lines = (0..height).map(|_| VT100String::new()).collect::<Vec<VT100String>>();
 
         Pane {
             id: String::from(id),
-            x, y,
-            height, width,
+            x,
+            y,
+            height,
+            width,
             lines,
             cur_x: 0,
             cur_y: 0,
-            print_state: PrintState::default()
+            print_state: PrintState::default(),
         }
     }
 
@@ -151,12 +210,12 @@ impl Pane {
                     if self.cur_y >= self.height as usize {
                         self.cur_y = (self.height - 1) as usize;
                         self.lines.remove(0); // discard the topmost line of output
-                        self.lines.push(VT100String::new(""));
+                        self.lines.push(VT100String::new());
                     }
                 }
                 _ => {
                     let line = self.lines.get_mut(vert_line).unwrap();
-                    line.set(self.cur_x, c);
+                    line.set(self.cur_x, Glyph::new(c, self.print_state));
                     self.cur_x += 1;
                 }
             }
@@ -167,7 +226,7 @@ impl Pane {
 
     // A Handle for testing
     fn plaintext(&self) -> String {
-        self.lines.iter().map(|l| l.as_str().to_owned()).collect::<Vec<String>>().join("\n")
+        self.lines.iter().map(|l| l.to_str(&self.print_state).to_owned()).collect::<Vec<String>>().join("\n")
     }
 }
 
@@ -209,7 +268,7 @@ mod tests {
      */
     #[test]
     fn it_converts_simple_vt100_sgr_to_print_state() {
-        let code = "\x1b]33m";
+        let code = "\x1b[33m";
         let mut ps = PrintState::default();
         ps.apply_vt100(code).unwrap();
         assert_eq!(ps.foreground, Color::Yellow);
@@ -217,7 +276,7 @@ mod tests {
 
     #[test]
     fn it_converts_bold_vt100_sgr_to_print_state() {
-        let code = "\x1b]93m";
+        let code = "\x1b[93m";
         let mut ps = PrintState::default();
         ps.apply_vt100(code).unwrap();
         assert_eq!(ps.foreground, Color::Yellow);
@@ -226,7 +285,7 @@ mod tests {
 
     #[test]
     fn it_converts_background_vt100_sgr_to_print_state() {
-        let code = "\x1b]43m";
+        let code = "\x1b[43m";
         let mut ps = PrintState::default();
         ps.apply_vt100(code).unwrap();
         assert_eq!(ps.background, Color::Yellow);
@@ -234,7 +293,7 @@ mod tests {
 
     #[test]
     fn it_converts_256_color_vt100_sgr_to_print_state() {
-        let code = "\x1b]38;5;128m";
+        let code = "\x1b[38;5;128m";
         let mut ps = PrintState::default();
         ps.apply_vt100(code).unwrap();
         assert_eq!(ps.foreground, Color::TWOFIFTYSIX(128));
@@ -242,10 +301,21 @@ mod tests {
 
     #[test]
     fn it_converts_rgb_color_vt100_sgr_to_print_state() {
-        let code = "\x1b]38;2;128;42;255m";
+        let code = "\x1b[38;2;128;42;255m";
         let mut ps = PrintState::default();
         ps.apply_vt100(code).unwrap();
         assert_eq!(ps.foreground, Color::RGB(128, 42, 255));
+    }
+
+    #[test]
+    fn it_converts_state_back_into_vt100() {
+        let fg_code = "\x1b[38;2;128;42;255m";
+        let bg_code = "\x1b[47m";
+        let mut ps = PrintState::default();
+        ps.apply_vt100(fg_code).unwrap();
+        ps.apply_vt100(bg_code).unwrap();
+
+        assert_eq!(ps.to_str(), fg_code.to_owned()+bg_code);
     }
 }
 
--- src/rex/terminal/vt100_string.rs
+++ src/rex/terminal/vt100_string.rs
@@ -1,143 +1,113 @@
-use std::ops::{Deref, Range};
 use lazy_static::lazy_static;
-use regex::{Regex, Match};
-use std::cmp::{min, max};
-use std::borrow::Cow;
+use regex::{Regex};
+use std::cmp::{max};
+use crate::rex::terminal::pane::PrintState;
 
 pub struct VT100String {
-    plain_str: String,
-    index_map: Vec<usize>
+    glyphs: Vec<Glyph>
 }
 
-impl Deref for VT100String {
-    type Target = String;
+#[derive(Copy, Clone)]
+pub struct Glyph {
+    c: char,
+    state: PrintState
+}
 
-    fn deref(&self) -> &Self::Target {
-        return &self.plain_str;
+impl Glyph {
+    pub fn new(c: char, state: PrintState) -> Self {
+        Glyph { c, state }
     }
 }
+
+impl Default for Glyph {
+    fn default() -> Self {
+        Glyph {
+            c: ' ',
+            state: PrintState::default()
+        }
+    }
+}
+
 lazy_static! {
     static ref VT100_REGEX: Regex = Regex::new(r"((\u001b\[|\u009b)[\u0030-\u003f]*[\u0020-\u002f]*[\u0040-\u007e])+").unwrap();
 }
 
 impl VT100String {
 
-    pub fn new(s: &str) -> VT100String {
+    pub fn new() -> VT100String {
         VT100String {
-            plain_str: String::from(s),
-            index_map: VT100String::build_index_map(s)
+            glyphs: Vec::new()
         }
     }
 
-    pub fn set(&mut self, index: usize, c: char) {
-        let extra_chars_reqd = max(0, index as i32 - (self.index_map.len() as i32 - 1));
+    pub fn set(&mut self, index: usize, g: Glyph) {
+        let extra_chars_reqd = max(0, index as i32 - (self.glyphs.len() as i32 - 1));
         println!("{} extra chars required to insert at {}", extra_chars_reqd, index);
         for _ in 0..extra_chars_reqd {
-            self.plain_str.push(' ');
+            let state = self.glyphs.last().unwrap_or(&Glyph::default()).state;
+            self.glyphs.push(Glyph::new(' ', state));
         }
 
-        if extra_chars_reqd > 0 {
-            self.index_map = VT100String::build_index_map(&self.plain_str);
-        }
-
-        let real_idx = self.index_map[index];
-        self.plain_str.replace_range(real_idx..real_idx, &c.to_string());
+        self.glyphs[index] = g;
     }
 
-    fn build_index_map(s: &str) -> Vec<usize> {
-        let length = s.len();
-        let mut out_vec: Vec<usize> = Vec::new();
-
-        if s.is_empty() { return out_vec; }
-
-        // Merge neighboring VT100s into single ranges, then use the start/end
-        // to add indices to out_vec
-        let mut vt100s = VT100String::find_vt100s(s);
-
-        let mut ranges: Vec<Range<usize>> = Vec::new();
-        let mut cur_range = 0..length-1;
-
-        // The inverse ranges of our vt100s are our plain text sections.
-        while let Some(nextVT) = vt100s.pop() {
-            println!("VT100s: {:?}", nextVT.as_str());
-            cur_range.end = min(cur_range.end, nextVT.start());
-            ranges.push(cur_range.clone());
-            cur_range = nextVT.end()+1..length-1;
-        }
-
-        ranges.push(cur_range.clone());
-
-        println!("Ranges!\n{:?}", ranges);
-
-        for range in ranges {
-            for i in range.start..=range.end {
-                out_vec.push(i);
-            }
-        }
-
-        out_vec
-    }
+    // fn build_index_map(s: &str) -> Vec<usize> {
+    //     let length = s.len();
+    //     let mut out_vec: Vec<usize> = Vec::new();
+    //
+    //     if s.is_empty() { return out_vec; }
+    //
+    //     // Merge neighboring VT100s into single ranges, then use the start/end
+    //     // to add indices to out_vec
+    //     let mut vt100s = VT100String::find_vt100s(s);
+    //
+    //     let mut ranges: Vec<Range<usize>> = Vec::new();
+    //     let mut cur_range = 0..length-1;
+    //
+    //     // The inverse ranges of our vt100s are our plain text sections.
+    //     while let Some(nextVT) = vt100s.pop() {
+    //         println!("VT100s: {:?}", nextVT.as_str());
+    //         cur_range.end = min(cur_range.end, nextVT.start());
+    //         ranges.push(cur_range.clone());
+    //         cur_range = nextVT.end()+1..length-1;
+    //     }
+    //
+    //     ranges.push(cur_range.clone());
+    //
+    //     println!("Ranges!\n{:?}", ranges);
+    //
+    //     for range in ranges {
+    //         for i in range.start..=range.end {
+    //             out_vec.push(i);
+    //         }
+    //     }
+    //
+    //     out_vec
+    // }
 
     pub fn len(&self) -> usize {
         self.plaintext().len()
     }
 
-    pub fn slice(&self, from: usize, to: usize) -> &str {
-        if to == self.len() {
-            let true_start = self.index_map[from];
-            &self.plain_str[true_start..]
-        } else {
-            let true_start = self.index_map[from];
-            let true_end = self.index_map[to];
-            &self.plain_str[true_start..true_end]
-        }
+    pub fn slice(&self, from: usize, to: usize) -> String {
+        self.glyphs[from..to].iter().map(|g| g.c.to_string()).collect::<Vec<String>>().join(" ")
     }
 
-    pub fn plaintext(&self) -> Cow<str> {
-        VT100_REGEX.replace_all(self.plain_str.as_str(), "")
+    pub fn plaintext(&self) -> String {
+        self.glyphs.iter().map(|g| g.c.to_string()).collect::<Vec<String>>().join(" ")
     }
 
-    pub fn vt100codes(&self) -> Vec<&str> {
-        VT100String::find_vt100s(&self.plain_str).iter().map(|c| c.as_str()).collect()
-    }
-
-    fn find_vt100s(s: &str) -> Vec<Match> {
-        VT100_REGEX.find_iter(s).collect()
-    }
-
-    fn esc_aware_slice(&self, n: usize) -> (usize, String) {
-        let s = self.plain_str.as_str();
-        if n >= s.len() { return (s.len(), format!("{:width$}", s, width = s.len())) }
-
-        // Early return - no VT100 to skip!
-        let vt100s = VT100String::find_vt100s(&self.plain_str);
-        if vt100s.last().is_none() { return (n, s[0..n].to_string()); }
-
-        let mut captured_chars = 0;
-        let mut end = 0;
-
-        for c in vt100s.iter() {
-            if (captured_chars + 1) < n {  // cc+1 to avoid subtraction with overflow
-                let next_block_of_text_size = c.start() - end;
-                let next_incr = if captured_chars + next_block_of_text_size >= n {
-                    (captured_chars + next_block_of_text_size) - n
-                } else {
-                    next_block_of_text_size
-                };
-
-                captured_chars += next_incr;
-                end = c.end();
+    pub fn to_str(&self, current_state: &PrintState) -> String {
+        let mut current_state = current_state;
+        let mut s = String::new();
+        for g in &self.glyphs {
+            if g.state != *current_state {
+                s += &g.state.to_str();
             }
-        };
-
-        if captured_chars < n {
-            end += n - captured_chars; // grab any remaining characters we need
+            s.push(g.c);
         }
 
-        let slice_end = min(s.len(), end);
-        let sliced_str = s[0..slice_end].to_string();
-
-        (end, sliced_str)
+        s
     }
 }
 
@@ -145,50 +115,5 @@ impl VT100String {
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_does_nothing_to_non_vt100_plain_text() {
-        let plaintext = VT100String::new("TEST");
-        assert_eq!(plaintext.index_map, vec![0, 1, 2, 3])
-    }
 
-    #[test]
-    fn it_keeps_preceding_vt100_values() {
-        let plaintext = VT100String::new("\x1b[33mTEST");
-        assert_eq!(plaintext.index_map, vec![0, 6, 7, 8])
-    }
-
-    #[test]
-    fn it_creates_indices_for_each_char() {
-        let plaintext = VT100String::new("\x1b[33mTEST");
-        assert_eq!(plaintext[plaintext.index_map[0]..(plaintext.index_map[1])], *"\x1b[33mT");
-        assert_eq!(plaintext[plaintext.index_map[1]..(plaintext.index_map[2])], *"E");
-        assert_eq!(plaintext[plaintext.index_map[2]..(plaintext.index_map[3])], *"S");
-        assert_eq!(plaintext[plaintext.index_map[3]..], *"T");
-    }
-
-    #[test]
-    fn it_indices_mid_vt100_as_expected() {
-        let plaintext = VT100String::new("TE\x1b[33mST");
-        assert_eq!(plaintext.index_map, vec![0, 1, 2, 8])
-    }
-
-    #[test]
-    fn it_merges_multiple_vt100_sequences() {
-        let plaintext = VT100String::new("TE\x1b[33m\x1b[HST");
-        assert_eq!(plaintext.index_map, vec![0, 1, 2, 11])
-    }
-
-    #[test]
-    fn it_slices_taking_vt100_into_account() {
-        let vt100text = VT100String::new("TE\x1b[33m\x1b[HST");
-        assert_eq!(vt100text.slice(2, vt100text.len()), "\x1b[33m\x1b[HST");
-        assert_eq!(vt100text.slice(3, vt100text.len()), "T");
-    }
-
-    #[test]
-    fn it_replaces_chars_in_mid_str() {
-        let mut vt100text = VT100String::new("TE\x1b[33m\x1b[HST");
-        vt100text.set(2, 's');
-        assert_eq!(vt100text.plain_str, "TE\x1b[33m\x1b[HsT")
-    }
 }
